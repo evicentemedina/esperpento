39674989d0031b2e1ea922bfb4a7d19766373600
Emilio Vicente Medina <evicentemedina@gmail.com>
2018-04-28 14:32:19 +0200

    [Android] First Volley working implementation

--- src/android/app/src/main/java/evicentemedina/esperpento/LoginActivity.java
+++ src/android/app/src/main/java/evicentemedina/esperpento/LoginActivity.java
@@ -10,6 +10,17 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import evicentemedina.esperpento.objects.Constants;
+import evicentemedina.esperpento.objects.VolleySingleton;
 
 public class LoginActivity extends AppCompatActivity
         implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
@@ -37,13 +48,28 @@ public class LoginActivity extends AppCompatActivity
     @Override
     public void onClick(View v) {
         int id = v.getId();
-
         if(id == R.id.loginBtnLogin) {
             String user = etUser.getText().toString(),
-                    pass = etPass.getText().toString();
+                   pass = etPass.getText().toString();
             if(!user.isEmpty() && !pass.isEmpty()) {
-                startActivity(new Intent(v.getContext(), MainActivity.class));
-                finish();
+                final View fv = v;
+                String url = Constants.URL+"login.php?u="+user+"&p="+pass;
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Toast.makeText(fv.getContext(), response.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(fv.getContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                );
+                VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+                //startActivity(new Intent(v.getContext(), MainActivity.class));
+                //finish();
             }
         }else if(id == R.id.loginBtnSignin) {
             startActivity(new Intent(v.getContext(), SignInActivity.class));
---
+++ src/android/app/src/main/java/evicentemedina/esperpento/objects/Constants.java
@@ -0,0 +1,7 @@
+package evicentemedina.esperpento.objects;
+
+public final class Constants {
+    private static final String URL_DEV = "http://192.168.1.2/esperpento/";
+    private static final String URL_PROD = "http://esperpento.ddns.net/esperpento/";
+    public static final String URL = URL_DEV;
+}
