7539db0651ddbf643ffb9b256e59987d1a1be77e
Emilio Vicente Medina <evicentemedina@gmail.com>
2018-04-28 14:42:55 +0200

    [Android] Changed Login layout to ScrollView & AppTheme with default Toolbar

--- src/android/app/src/main/java/evicentemedina/esperpento/LoginActivity.java
+++ src/android/app/src/main/java/evicentemedina/esperpento/LoginActivity.java
@@ -3,7 +3,6 @@ package evicentemedina.esperpento;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
@@ -31,8 +30,6 @@ public class LoginActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         etUser = findViewById(R.id.loginEtUser);
         etPass = findViewById(R.id.loginEtPass);
