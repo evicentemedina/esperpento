b83ea51c5b09c9252774156ea8d3af9ed931ca87
Emilio Vicente Medina <evicentemedina@gmail.com>
2018-05-19 17:25:35 +0200

    [PHP] Added get_threads_comm_recent & mod get_threads_usr_recent

--- src/php/get_threads_comm_recent.php
+++ src/php/get_threads_comm_recent.php
@@ -1,12 +1,10 @@
 <?php
-if(isset($_GET['u'])){
+if(isset($_GET['c'])){
   include 'conn.php';
   $sql = <<<SQL
-    select * from threads where community in(
-      select community from users_communities where "user"=$1
-    ) order by "time" desc limit 50
+    select * from threads where community=$1 order by "time" desc
 SQL;
-  $res = pg_query_params($con, $sql, array($_GET['u']));
+  $res = pg_query_params($con, $sql, array($_GET['c']));
   if(pg_num_rows($res) > 0){
     $response['s'] = 1;
     $response['c'] = array();
