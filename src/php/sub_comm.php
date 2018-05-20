88bddba7cecdf43db72a25ba9771919ea4276999
Emilio Vicente Medina <evicentemedina@gmail.com>
2018-05-20 02:16:17 +0200

    [PHP] Changed GET param "name" to "c" on sub_comm

--- src/php/sub_comm.php
+++ src/php/sub_comm.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET["u"]) && isset($_GET["p"]) && isset($_GET["name"])){
+if(isset($_GET["u"]) && isset($_GET["p"]) && isset($_GET["c"])){
   include 'conn.php';
   $res = pg_query_params(
     $con,
@@ -10,18 +10,18 @@ if(isset($_GET["u"]) && isset($_GET["p"]) && isset($_GET["name"])){
     $res = pg_query_params(
       $con,
       "select 1 from communities where name=$1",
-      array($_GET["name"])
+      array($_GET["c"])
     );
     if(pg_num_rows($res) == 1){
       $res = pg_query_params(
         $con,
         'select 1 from users_communities where "user"=$1 and community=$2',
-        array($_GET["u"], $_GET["name"])
+        array($_GET["u"], $_GET["c"])
       );
       if(pg_num_rows($res) == 0){
         if(pg_insert($con, "users_communities", array(
           'user' => $_GET["u"],
-          'community' => $_GET["name"]
+          'community' => $_GET["c"]
         ))){
           $response["s"] = 1;
           $response["c"] = 1;
@@ -29,7 +29,7 @@ if(isset($_GET["u"]) && isset($_GET["p"]) && isset($_GET["name"])){
       }else{
         if(pg_delete($con, "users_communities", array(
           'user' => $_GET["u"],
-          'community' => $_GET["name"]
+          'community' => $_GET["c"]
         ))){
           $response["s"] = 1;
           $response["c"] = 0;
