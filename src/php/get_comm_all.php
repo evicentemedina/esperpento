db40032f5fe30a0cd37b451d9944ec257ca3e054
Emilio Vicente Medina <evicentemedina@gmail.com>
2018-05-26 00:02:50 +0200

    [PHP] Improved get_comm_all query

--- src/php/get_comm_all.php
+++ src/php/get_comm_all.php
@@ -1,6 +1,11 @@
 <?php
 include 'conn.php';
-$res = pg_query($con, "select * from communities");
+$sql = <<<SQL
+  select name,descrip,"time",icon,color,bg_color,admin,(
+    select count(community) from users_communities where community=name
+  ) from communities order by count desc
+SQL;
+$res = pg_query($con, $sql);
 if(pg_num_rows($res) > 0){
   $response['s'] = 1;
   $response['c'] = array();
