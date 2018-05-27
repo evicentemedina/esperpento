2fd4b21b62a68c71930cf0d73864925a679a8ac2
Emilio Vicente Medina <evicentemedina@gmail.com>
2018-05-27 22:20:44 +0200

    [PHP] Modified to match new comment votes functionality

--- src/php/get_thread_comments.php
+++ src/php/get_thread_comments.php
@@ -1,14 +1,16 @@
 <?php
-if(isset($_GET['t'])){
+if(isset($_GET['t']) && isset($_GET['u'])){
   include 'conn.php';
   $sql = <<<SQL
     select id,content,"time",thread,"user",((
       select count(comment) from votes_comments where comment=id and vote=true
     )-(
       select count(comment) from votes_comments where comment=id and vote=false
-    )) as votes from comments where thread=$1 order by votes desc
+    )) as votes,(
+      select vote from votes_comments where "user"=$2 and comment=id
+    ) from comments where thread=$1 order by votes desc
 SQL;
-  $res = pg_query_params($con, $sql, array($_GET['t']));
+  $res = pg_query_params($con, $sql, array($_GET['t'], $_GET['u']));
   if(pg_num_rows($res) > 0){
     $response['s'] = 1;
     $response['c'] = array();
--- src/php/ins_vote_comment.php
+++ src/php/ins_vote_comment.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET['u']) && isset($_GET['p']) && isset($_GET['c']) && isset($_GET['v'])){
+if(isset($_GET['u']) && isset($_GET['p']) && isset($_GET['c'])){
   include 'conn.php';
   $res = pg_query_params(
     $con,
@@ -9,33 +9,38 @@ if(isset($_GET['u']) && isset($_GET['p']) && isset($_GET['c']) && isset($_GET['v
   if(pg_num_rows($res) == 1){
     $res = pg_query_params($con, 'select 1 from comments where id=$1', array($_GET['c']));
     if(pg_num_rows($res) == 1){
-      if($_GET['v'] == -1){
+      $res = pg_query_params(
+        $con,
+        'select vote from votes_comments where "user"=$1 and comment=$2',
+        array($_GET['u'], $_GET['c'])
+      );
+      $vote = 1;
+      if(pg_num_rows($res) == 1){
+        $row = pg_fetch_row($res)[0];
+        if($row == "t"){
+          $vote = 0;
+        }else{
+          $vote = -1;
+        }
         if(pg_delete($con, "votes_comments", array(
           'user' => $_GET['u'],
           'comment' => $_GET['c']
         ))){
           $response['s'] = 1;
         }
-      }else if($_GET['v'] == 0 || $_GET['v'] == 1){
-        $res = pg_query_params(
-          $con, 
-          'select 1 from votes_comments where "user"=$1 and comment=$2',
-          array($_GET['u'], $_GET['c'])
-        );
-        if(pg_num_rows($res) == 1){
-          pg_delete($con, "votes_comments", array(
-            'user' => $_GET['u'],
-            'comment' => $_GET['c']
-          ));
-        }
+      }
+      if($vote > -1){
         if(pg_insert($con, "votes_comments", array(
           'user' => $_GET['u'],
           'comment' => $_GET['c'],
-          'vote' => $_GET['v']
+          'vote' => $vote
         ))){
           $response['s'] = 1;
+        }else{
+          $response['s'] = 0;
         }
       }
+      $response['c'] = $vote;
     }
   }
   include 'close.php';
