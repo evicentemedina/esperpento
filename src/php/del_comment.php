aed08c7a3f1577f1ca2b3865e2f85325217f8a16
Emilio Vicente Medina <evicentemedina@gmail.com>
2018-06-03 13:40:08 +0200

    [PHP] Fixed comment author not being checked

--- src/php/del_comment.php
+++ src/php/del_comment.php
@@ -7,7 +7,8 @@ if(isset($_GET['u']) && isset($_GET['p']) && isset($_GET['c'])){
   );
   if(pg_num_rows($res) == 1){
     $res = pg_query_params(
-      $con, 'select 1 from comments where id=$1', array($_GET['c'])
+      $con, 'select 1 from comments where id=$1 and "user"=$2',
+      array($_GET['c'], $_GET['u'])
     );
     if(pg_num_rows($res) == 1){
       if(pg_delete($con, "comments", array('id' => $_GET['c']))){
